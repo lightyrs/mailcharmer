4c58ca8b76209e844c48a065c54afca14d94a0c7
Harris Novick <harrisdavidnovick@gmail.com>
2014-03-14 02:31:48 -0400

    wire up mysql adapter

--- config/adapters.js
+++ config/adapters.js
@@ -1,8 +1,8 @@
 /**
  * Global adapter config
- * 
+ *
  * The `adapters` configuration object lets you create different global "saved settings"
- * that you can mix and match in your models.  The `default` option indicates which 
+ * that you can mix and match in your models.  The `default` option indicates which
  * "saved setting" should be used if a model doesn't have an adapter specified.
  *
  * Keep in mind that options you define directly in your model definitions
@@ -14,9 +14,9 @@
 
 module.exports.adapters = {
 
-  // If you leave the adapter config unspecified 
+  // If you leave the adapter config unspecified
   // in a model definition, 'default' will be used.
-  'default': 'disk',
+  'default': 'mysql',
 
   // Persistent adapter for DEVELOPMENT ONLY
   // (data is preserved when the server shuts down)
@@ -24,16 +24,14 @@ module.exports.adapters = {
     module: 'sails-disk'
   },
 
-  // MySQL is the world's most popular relational database.
-  // Learn more: http://en.wikipedia.org/wiki/MySQL
-  myLocalMySQLDatabase: {
-
-    module: 'sails-mysql',
-    host: 'YOUR_MYSQL_SERVER_HOSTNAME_OR_IP_ADDRESS',
-    user: 'YOUR_MYSQL_USER',
-    // Psst.. You can put your password in config/local.js instead
-    // so you don't inadvertently push it up if you're using version control
-    password: 'YOUR_MYSQL_PASSWORD', 
-    database: 'YOUR_MYSQL_DB'
+  mysql: {
+    module   : 'sails-mysql',
+    host     : 'localhost',
+    port     : 3306,
+    user     : 'root',
+    password : 'password',
+    database : 'mailcharmer_dev',
+    module   : 'sails-mysql',
+    url      : 'mysql2://root:password@localhost:3306/mailcharmer_dev'
   }
 };
